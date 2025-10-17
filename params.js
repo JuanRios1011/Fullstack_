7391d06b4cf69be04031e64ac8f5efab1e6e9882
Juan Rios <160086767+JuanRios1011@users.noreply.github.com>
2025-10-16 19:39:17 -0500

    parametros

---
+++ params.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const url = require("url");
+
+const server = http.createServer((req, res)=>{
+    // URL parseada como Objeto
+    const parsedUrl = url.parse(req.url, true);
+
+    // Array de Parametros
+    const query = parsedUrl.query;
+
+    //http response - head
+    res.writeHead(200, {"content-type": "application/json"});
+
+    //http response - body
+    res.end(JSON.stringify(
+        {
+            message: "Parametros Recibidos",
+            params: query
+        }
+    ));
+});
+
+server.listen(3000, ()=>{
+    console.log("El servidor está corriendo en http://localhost:3000/")
+})
